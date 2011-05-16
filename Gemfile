7e33d28bac019dec365444c1185e61039985f016
Alan Holsztynski <aholsztynski@coletaylor.com>
2011-05-15 22:42:28 -0400

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,18 @@
+source 'http://rubygems.org'
+
+gem 'rails', ' 3.0.7'
+gem 'sqlite3', '1.3.3'
+# gem 'gravatar_image_tag', '1.0.0'
+# gem 'will_paginate', '3.0.pre2'
+
+group :development do
+	gem 'rspec-rails', '2.6.0'
+	gem 'annotate-models', '1.0.4'
+	# gem 'faker', '0.9.5'
+end
+
+group :test do
+	gem 'rspec', '2.6.0'
+	gem 'webrat', '0.7.3'
+	# gem 'factory_girl_rails', '1.0.1'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Luzco::Application.config.secret_token = '652894abd9ee92a4f6eae019f3e39a0ad2a57f42a115241bc507d3c5662a537db3f1a58f40353e64b943dae601f851bb4641822473d9aab8120d7aabb857390b'
